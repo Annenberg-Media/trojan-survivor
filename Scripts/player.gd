24e09e4217f043277e1dd82950a579708d28feb3
unknown <dwlee0802@gmail.com>
2025-03-02 17:13:07 -0800

    Basic shooting

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,23 +1,55 @@
 extends CharacterBody2D
 
+@onready
+var crosshair: Node2D = $Crosshair
+var bullet_prefab: PackedScene = preload("uid://pe8pcbqp47dd")
+var shoot_cooldown_timer: Timer
+@export
+var shooting_cooldown_amount: float = 0.2
+
 @export
 ## player's movement speed in pixels per second
-var movement_speed: float = 400
+var movement_speed: float = 200
 
+
+func _ready() -> void:
+	shoot_cooldown_timer = Timer.new()
+	shoot_cooldown_timer.autostart = false
+	shoot_cooldown_timer.one_shot = true
+	shoot_cooldown_timer.process_callback = Timer.TIMER_PROCESS_PHYSICS
+	add_child(shoot_cooldown_timer)
+	
+	
 func _physics_process(delta: float) -> void:
 	var move_dir: Vector2 = Vector2.ZERO
 	# W - up
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		move_dir += Vector2.UP
 	# A - left
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		move_dir += Vector2.LEFT
 	# S - down
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		move_dir += Vector2.DOWN
 	# D - right
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		move_dir += Vector2.RIGHT
 	move_dir = move_dir.normalized()
 	
 	global_position += move_dir * (movement_speed * delta)
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot_projectile(global_position.direction_to(crosshair.global_position))
+
+
+func shoot_projectile(dir: Vector2):
+	# shooting is on cooldown
+	if !shoot_cooldown_timer.is_stopped():
+		print("Shooting on cooldown")
+		return
+		
+	var new_projectile: Projectile = bullet_prefab.instantiate()
+	new_projectile.global_position = global_position
+	new_projectile.direction = dir
+	get_tree().root.add_child(new_projectile)
+	shoot_cooldown_timer.start(shooting_cooldown_amount)
---
+++ Scripts/projectile.gd
@@ -0,0 +1,25 @@
+extends Area2D
+class_name Projectile
+
+@export
+## speed in pixels per second
+var speed: float = 500
+
+var direction: Vector2 = Vector2.ZERO
+
+## lifetime to auto free stray bullets
+var lifetime: float = 10
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	## sprite randomization
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	global_position += direction * (speed * delta)
+	lifetime -= delta
+	if lifetime < 0:
+		queue_free()
