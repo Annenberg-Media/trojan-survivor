df4458cddd70003840fd2def1adcaa95f2bff594
Jon Moon <jonmoon@Mac.attlocal.net>
2025-03-15 15:26:46 -0700

    implemented random spawn locations for enemies, just outside of camera visible range

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -30,8 +30,39 @@ func spawn_enemy():
 	if enemy1 == null:
 		print("ERROR: Enemy instantiation failed")
 		return 
-	enemy1.position = $"../Player".position + Vector2(200, 200)
-	enemy2.position = $"../Player".position + Vector2(-200, -200)
+	enemy1.position = enemy_spawn_position()
+	enemy2.position = enemy_spawn_position()
 	
 	add_child(enemy1)
 	add_child(enemy2)
+	
+func enemy_spawn_position():
+	# Generates a random enemy spawn position, just outside of camera visible range
+	
+	# Camera and viewport related variables
+	var camera = get_viewport().get_camera_2d()
+	var viewport_size = get_viewport().size
+	var camera_position = camera.global_position
+	
+	# Determine random spawn position
+	var spawn_offset = 100 # pixel distance beyond visible range
+	var spawn_side = randi() % 4 # 0=Left, 1=Right, 2=Top, 3=Bottom
+	var spawn_position = Vector2.ZERO
+	
+	match spawn_side:
+		0:
+			spawn_position = Vector2(camera_position.x - viewport_size.x/2 - spawn_offset, 
+			randf_range(camera_position.y - viewport_size.y/2, camera_position.y + viewport_size.y/2))
+		1:
+			spawn_position = Vector2(camera_position.x + viewport_size.x/2 + spawn_offset, 
+			randf_range(camera_position.y - viewport_size.y/2, camera_position.y + viewport_size.y/2))
+		2:
+			spawn_position = Vector2(randf_range(camera_position.x - viewport_size.x/2, camera_position.x + viewport_size/2),
+			camera_position.y - viewport_size.y/2 - spawn_offset)
+		3:
+			spawn_position = Vector2(randf_range(camera_position.x - viewport_size.x/2, camera_position.x + viewport_size.x/2),
+			camera_position.y + viewport_size.y/2 + spawn_offset)
+			
+	return spawn_position
+	
+	
