99deffba9af3cf18ece9cc6e25cad267e6c813d4
Jon Moon <jonmoon@Mac.attlocal.net>
2025-04-06 00:39:50 -0700

    added decreasing enemy spawn timers

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -13,6 +13,10 @@ var rng = RandomNumberGenerator.new()
 var game_time: float = 0
 var score: int = 0
 
+var past_twenty = false
+var past_forty = false
+var past_sixty = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if enemy_1_scene == null:
@@ -25,14 +29,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	'''
-	=== Temporary code of timer getting faster and faster to spawn enemies ===
-	faster as game progresses. It technically works, but I'm almost certain 
-	that there will be a more elegant way to do this; will look into later :)
-	if spawnTimer.wait_time > 1:
-		spawnTimer.wait_time -= 0.1 * delta
-	'''
-	# pass
+	# calls function to check if certain in-game times have been met 
+	# to decrease enemy spawn timer
+	check_decrement_spawn_timer(Time.get_ticks_msec()/1000.0)
+		
 	game_time += delta
 	emit_signal("time_changed", game_time)
 	
@@ -85,8 +85,26 @@ func _on_enemy_spawn_timer_timeout() -> void:
 	var num_enemies = randi_range(1, 3)
 	print("Timer end; spawning ", num_enemies, " enemies!")
 	spawn_enemy(num_enemies)
+	print("Spawning more enemies in: ", spawnTimer.wait_time, " seconds")
 	
 	
 func add_score(points: int) -> void:
 	score += points
 	emit_signal("score_changed", score)
+	
+func check_decrement_spawn_timer(game_time: float):
+	if game_time >= 20 and not past_twenty:
+		print("DEBUG: player survived 20 seconds. decrementing timer")
+		past_twenty = true
+		spawnTimer.wait_time -= 0.5
+		
+	if game_time >= 40 and not past_forty:
+		print("DEBUG: player survived 40 seconds. decrementing timer further")
+		past_forty = true
+		spawnTimer.wait_time -= 0.5
+		
+	if game_time >= 60 and not past_sixty:
+		print("DEBUG: player survived a minute. decrementing timer")
+		past_sixty = true
+		spawnTimer.wait_time -= 0.5
+	
