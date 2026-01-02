fe0de226771f42a626a661436fb692570fc0471e
jmoon98 <jihwanmo@usc.edu>
2026-01-02 08:21:05 -0800

    player projectile sprites and sprite randomization

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -9,16 +9,19 @@ var direction: Vector2 = Vector2.ZERO
 
 ## lifetime to auto free stray bullets
 var lifetime: float = 10
+var sprite_num 
+var projectile_sprite
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	## sprite randomization
-	pass
-
-
+	projectile_sprite = $AnimatedSprite2D
+	sprite_num = str(randi_range(1, 9))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	projectile_sprite.play(sprite_num)
+	rotation += 3*delta
 	global_position += direction * (speed * delta)
 	lifetime -= delta
 	if lifetime < 0:
